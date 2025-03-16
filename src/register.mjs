316723d2ec19793e8bbc428e12ba01c59e2aad8f
haruyuki@hailfall.macbook <haruyuki@relicsnow.info>
2025-03-17 00:33:03 +0900

    7.3.

--- src/register.mjs
+++ src/register.mjs
@@ -2,6 +2,10 @@ import { ClassEditPage } from "./pages/class-edit.mjs";
 import { ClassListPage } from "./pages/class-list.mjs";
 import { HomePage } from "./pages/home.mjs";
 
+import { ClassListItemComponent } from "./components/class-list-item.mjs";
+
 customElements.define("home-page", HomePage);
 customElements.define("class-list-page", ClassListPage);
 customElements.define("class-edit-page", ClassEditPage);
+
+customElements.define("class-list-item", ClassListItemComponent);
